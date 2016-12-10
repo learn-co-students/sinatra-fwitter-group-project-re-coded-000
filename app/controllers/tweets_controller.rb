70ae2eae02931846ce02433f9fcd25f6aa4b3ea2
Ali Alrawi <ali.developer90@gmail.com>
2016-12-11 01:46:42 +0300

    almost Done

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,14 +43,27 @@ class TweetsController < ApplicationController
       end 
     end
     patch '/tweets/:id' do 
-        if params[:content] !=""
-           @tweet=Tweet.find_by_id(params[:id])
-           @tweet.content =params[:content]
-           @tweet.save
-           redirect to "/tweets/#{@tweet.id}"
-        else
-        	redirect to "/tweets/#{@tweet.id}/edit" 
-        end
-
+     if params[:content] == ""
+      redirect to "/tweets/#{params[:id]}/edit"
+     else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+     end
     end
+   delete '/tweets/:id/delete' do 
+      if session[:user_id]	!=nil 
+         @tweet=Tweet.find_by_id(params[:id])
+        if @tweet.user_id==session[:user_id] 
+        	@tweet.delete
+        	redirect to "/tweets"
+        else
+        redirect to "/tweets"
+
+        end	
+      else
+        redirect to "/login"  
+      end 
+  end
 end
