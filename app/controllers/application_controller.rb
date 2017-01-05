929e82298a3549e76233a9217a20be4d70422767
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-05 13:29:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,5 @@ class ApplicationController < Sinatra::Base
    def current_user
      User.find_by_id(session[:user_id])
    end
-end
-
+ end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   get '/signup' do
     if !logged_in? then
-    erb :'users/create_user'
-  else
-    redirect '/tweets'
-  end
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect '/tweets'
+    end
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    if params[:username] == " " || params[:email] == " " || params[:password] == " "
       redirect to '/signup'
     else
-      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
       session[:user_id] = @user.id
       redirect to '/tweets'
     end
